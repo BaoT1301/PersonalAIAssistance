4f74918b2fd0d165befe40e24a87007cec2e7211
Bao Tran <baot130105@gmail.com>
2025-12-20 22:55:16 -0500

    Remove unused import

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
 from tools import search_tool, wiki_tool, save_tool
 import time
-import sys
 
 load_dotenv()
 
